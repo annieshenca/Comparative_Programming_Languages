fcef51e9115f5641c3e201a9098b894f49ef29dc
Annie Shen <annieshenca@gmail.com>
2018-04-26 19:49:46 -0700

    update

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -48,9 +48,9 @@ module Bigint = struct
 
     (* Pass in a list and link each number in the whole list together
      * to form a total number *)
-    let list_concat list =
+    let list_concat list1 =
         float_of_string (String.concat ""
-            (List.rev_map string_of_int list))
+            (List.rev_map string_of_int list1))
 
 
 (* ///////////////// *)
@@ -72,9 +72,9 @@ module Bigint = struct
     let add (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         (* If the +/- symbols are the same, then pass in  *)
         if neg1 = neg2
-            then Bigint (neg1, add' value1 value2 0)
+        then Bigint (neg1, add' value1 value2 0)
         else if list_concat value1 > list_concat value2
-            then Bigint (neg1, add' value1 value2 0)
+        then Bigint (neg1, add' value1 value2 0)
         else Bigint (neg2, add' value2 value1 0)
 
 (* ///////////////// *)
